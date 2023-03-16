ad8f73111a3e4d5d8c09d1853812c8a2114ac80a
Joffreynk <joyjoffrey@gmail.com>
2023-03-16 23:14:36 +0200

    testing routing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import  Express  from "express";
+import  express  from "express";
 
-const app = Express();
+const app = express();
+
+app.get("/", (req, res)=>{
+  res.status(201).json({name:"Joffrey", pwd:"123456"})
+})
 
 app.listen(8000, () => {
   console.log("API server listening on port 8000");
