6f7bc39ce01a9ac1e6bbde4ec1b20137e92b6c13
Joffreynk <joyjoffrey@gmail.com>
2023-03-17 20:17:54 +0200

    add credentials cors

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import  express  from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -10,10 +12,9 @@ import userDetailsRouter from "./routes/userDetails.js"
 import storiesRouter from "./routes/stories.js"
 import authRouter from "./routes/auth.js"
 
-
 app.use(express.json())
-// app.use(express.cors())
-
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", usersRouter);
