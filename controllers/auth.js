056e99044bb6fd973adb877e00fa89f427a23f2a
Joffreynk <joyjoffrey@gmail.com>
2023-03-17 21:31:20 +0200

    add token to the app

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,28 @@
 import bcrypt from "bcryptjs";
-import db from "../models/connection.js"
+import jwt from 'jsonwebtoken';
+import db from "../models/connection.js";
 
 export const login = (req, res)=>{
 
-  const {userName, password} = req.body;
     
-  const hashedpassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const hashedpassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 
-  const sql = "select * from users where userName = ?"
+  const sql = "select * from users where userName = ?";
 
-  db.query(sql, [userName, hashedpassword], (error, data)=>{
+  db.query(sql, [req.body.userName], (error, data)=>{
     if (error) return res.status(500).json(error);
 
     if(data.length>0){
-      const comparedPassword = bcrypt.compareSync(password, data[0].password)
-      if(!comparedPassword) return res.status(404).json({message: "wrong UserName or Password"}) 
-      return res.status(201).json(data[0])
+
+      const token = jwt.sign({id: data[0].id}, "seacretKey")
+
+      const comparedPassword = bcrypt.compareSync(req.body.password, data[0].password)
+
+      
+      if(!comparedPassword) return res.status(404).json({message: "wrong UserName or Password"});
+
+      const {password, ...others} = data[0]
+      return res.cookie("accessToken", token, {httpOnly: true}).status(200).json(others)
     }else {
       return res.status(404).json({message: "user not found"})
     }
