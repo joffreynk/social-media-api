0833da6a0f5917e877d302a3db7c8a191fdc9deb
Joffreynk <joyjoffrey@gmail.com>
2023-03-17 13:56:17 +0200

    add create user functionality

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,56 @@
+import bcrypt from "bcryptjs";
+import db from "../models/connection.js";
 
 export const getUsers = (req, res)=>{
-  res.status(201).json({name:"Joffrey", pwd:"123456"})
+      
+  // check if user exist
+  const sqlCheck = "SELECT * FROM users";
+
+  db.query(sqlCheck, [req.body.user], (err, data)=>{
+    if(err) return res.status(500).json(err);
+    if(data.length) return res.status(409).json(data);
+})
 }
 
 
 export const addUser = (req, res)=>{
-  res.status(201).json({name:"Joffrey", pwd:"123456"})
+  // check if user exist
+  const sqlCheck = "SELECT * FROM users WHERE userName = ?";
+
+  db.query(sqlCheck, [req.body.userName], (err, data)=>{
+    if(err) return res.status(500).json(err);
+    if(data.length) return res.status(409).json({message: "user already exists!"});
+
+    // IF USER DOESN'T EXIST CREATE NEW ONE
+    const sql = "INSERT INTO users (userName, lastName, firstName, email, password, profilePicture, coverPicture, location) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+    const {userName, lastName, firstName, email, password, profilePicture, coverPicture, location}  = req.body;
+    
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+    const values = [userName, lastName, firstName, email, hashedPassword, profilePicture, coverPicture, location];
+
+    db.query(sql, values, (error, dataValues) => {
+      if(error) return res.status(500).json(error);
+
+      return res.status(200).json({message: "User created successfully!"});
+    })
+  })
+
 }
 
+
+
 export const getUser = (req, res)=>{
-  res.status(201).json({name:"Joffrey", pwd:"123456"})
+  res.status(201).json({name:"Joffrey", pwd:"123456"});
 }
 
 
 export const deleteUser = (req, res)=>{
-  res.status(201).json({name:"Joffrey", pwd:"123456"})
+  res.status(201).json({name:"Joffrey", pwd:"123456"});
 }
 
 
 export const updateUser = (req, res)=>{
-  res.status(201).json({name:"Joffrey", pwd:"123456"})
+  res.status(201).json({name:"Joffrey", pwd:"123456"});
 }
 
