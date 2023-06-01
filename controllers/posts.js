632126f58e64b9587b890889959c9f5c3d37f3d0
Joffreynk <joyjoffrey@gmail.com>
2023-06-01 22:06:06 +0200

    add upload post

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,6 +11,7 @@ export const getPosts = (req, res)=>{
   jwt.verify(token, "secretKey", (err, mytoken)=>{
     if(err) return res.status(500).json({message:"Invalid token"})
 
+
     db.query(sql, [mytoken.id, mytoken.id], (err,data)=>{
       if (err) return res.status(404).json({message:err});
       return  res.status(201).json(data)
@@ -22,8 +23,11 @@ export const getPosts = (req, res)=>{
 
 export const addPost = (req, res)=>{
 
+  const token = req.cookies.socialMediaAppToken;
+  console.log('create post called');
+
    if (!token) return res.status(401).json({message: "Not logged in"})
-  const sql = "INSERT INTO Posts () VALUES ()"; // 
+  const sql = "INSERT INTO Posts () VALUES ()";
 
   jwt.verify(token, "secretKey", (err, mytoken)=>{
     if(err) return res.status(500).json({message:"Invalid token"})
@@ -33,12 +37,20 @@ export const addPost = (req, res)=>{
         if(err) return res.status(500).json({message:"failed to upload post image"})
       })
 
-      const {path, filenames} = req.files;
       
-      db.query(sql, [mytoken.id, mytoken.id], (err,data)=>{
-        if (err) return res.status(404).json({message:err});
-        return  res.status(201).json(data)
-      })
+    console.log('ready to create user');
+    console.log(req.body);
+    
+    
+    
+    const {path, filename} = req.files;
+    console.log(filename);
+    
+    // db.query(sql, [mytoken.id, mytoken.id], (err,data)=>{
+      //   if (err) return res.status(404).json({message:err});
+      //   return  res.status(201).json(data)
+      // })
+      return res.status(200).json({message:"ready to create user"})
     } catch (error) {
       
     }
