8c2f8842edfcf76458a526ace4c2a990e42a4c1a
Joffreynk <joyjoffrey@gmail.com>
2023-06-10 20:20:13 +0200

    create follow controller

---
+++ controllers/follow.js
@@ -0,0 +1,16 @@
+
+export const FollowaUser = (req, res)=>{
+  res.status(201).json({name:"likes", pwd:"123456"})
+}
+
+  
+
+export const FollowBackUser = (req, res)=>{
+  res.status(201).json({name:"likes", pwd:"123456"})
+}
+
+
+
+export const unFollow = (req, res)=>{
+  res.status(201).json({name:"likes", pwd:"123456"})
+}
