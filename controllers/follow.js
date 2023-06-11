efb24a0055b42a439b130eba0e9aa8cf18486629
Joffreynk <joyjoffrey@gmail.com>
2023-06-11 11:30:04 +0200

    get sql that return all followers

--- controllers/follow.js
+++ controllers/follow.js
@@ -15,14 +15,23 @@ export const followUser = (req, res)=>{
     })
 
   });
-
-  res.status(201).json({name:"likes", pwd:"123456"})
 }
 
   
 
 export const followBackUser = (req, res)=>{
-  res.status(201).json({name:"likes", pwd:"123456"})
+  const token  = req.headers.token;
+  if (!token) return res.status(403).json({message: 'please sign in again'});
+
+  Jwt.verify(token, "secretKey", (err, myToken)=>{
+    if(err) res.status(404).json({message:'Invalid token' });
+    const sql = 'UPDATE Follow SET followBack = 1 WHERE followedId = ? AND id = ?';
+
+    db.query(sql, [myToken.id, req.body.id], (err, result)=>{
+      if(err) res.status(404).json({message: err.message});
+      return res.status(200).json({message: 'you have successfully followed'});
+    })
+  });
 }
 
 
@@ -32,5 +41,16 @@ export const unFollow = (req, res)=>{
 }
 
 export const getFollowers = (req, res)=>{
-  res.status(201).json({name:"likes", pwd:"123456"})
+  const token  = req.headers.token;
+  if (!token) return res.status(403).json({message: 'please sign in again'});
+
+  Jwt.verify(token, "secretKey", (err, myToken)=>{
+    if(err) res.status(404).json({message:'Invalid token' });
+    const sql = "SELECT  u.id AS userId, u.userName, u.lastName, u.firstName, u.email, u.profilePicture, u.coverPicture, u.location  FROM users AS u LEFT JOIN Follow AS f ON(u.id = f.followerId OR u.id != f.followedId) WHERE f.followerId != 8 OR f.followedId = 1;";
+
+    db.query(sql, [myToken.id, req.body.id], (err, result)=>{
+      if(err) res.status(404).json({message: err.message});
+      return res.status(200).json(result);
+    })
+  });
 }
